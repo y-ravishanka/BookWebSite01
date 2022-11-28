42145030a354d1feef19ae0aeaa2e41b1e573187
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-28 09:18:43 +0530

    remove old project files

---
+++ BookWebSite01/Client/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿
+const title = document.getElementById("index-title");
+const btn = document.getElementById("index-btn");
+
+btn.addEventListener("click", function () {
+    title.innerText = "Hi !!!, i am yasas";
+});
