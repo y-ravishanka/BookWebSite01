f1eb6a77372141456142fedcf2e49fd52cf78aec
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-28 09:22:35 +0530

    create new project files

---
+++ BookSiteServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookSiteServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
