23336c43afc67d7e8c8386425e6ef74a617ea7d0
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-05 10:53:19 +0530

    add new razor page Categories

---
+++ BookSiteServer/Views/Home/Categories.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookSiteServer.Views.Home
+{
+    public class CategoriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
