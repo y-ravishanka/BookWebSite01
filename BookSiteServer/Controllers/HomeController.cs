04767c45c9fa1a897dd06f6144ee2fa68038a61e
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-05 10:53:55 +0530

    add new added razor pages to the Home controller

--- BookSiteServer/Controllers/HomeController.cs
+++ BookSiteServer/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace BookSiteServer.Controllers
             return View();
         }
 
+        public IActionResult Categories()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
