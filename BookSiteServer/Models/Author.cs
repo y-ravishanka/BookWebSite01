2ed44cdc15ff885bc5020de0d5acad4fe7e796c8
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-09 14:37:29 +0530

    add author model class

---
+++ BookSiteServer/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSiteServer.Models
+{
+    public class Author
+    {
+        [Key]
+        public Guid AuthorId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<AuthorName>? AltName { get; set; }
+    }
+}
