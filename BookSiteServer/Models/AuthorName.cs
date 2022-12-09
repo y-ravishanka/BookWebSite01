320805f9c0d7396da0753891d4ab921229c0b05a
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-09 14:36:08 +0530

    add authorName model class to store alternate names of the author

---
+++ BookSiteServer/Models/AuthorName.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSiteServer.Models
+{
+    public class AuthorName
+    {
+        [Key]
+        public int AltNameId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
