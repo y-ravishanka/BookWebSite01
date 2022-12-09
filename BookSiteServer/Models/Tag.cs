be9fdcfca850908104a7ec2d5a56f1b72c565fa4
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-09 14:22:05 +0530

    add Tag model class

---
+++ BookSiteServer/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSiteServer.Models
+{
+    public class Tag
+    {
+        [Key]
+        public int TagId { get; set; }
+        
+        [Required]
+        public string TagName { get; set; }
+
+        public string? Description { get; set; }
+
+    }
+}
