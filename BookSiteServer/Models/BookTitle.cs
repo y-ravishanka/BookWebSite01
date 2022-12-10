0b7e54f932cd8c7db62983eb671100041841dc9f
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-10 07:14:15 +0530

    add bookTitle model class to contain alternative book titles

---
+++ BookSiteServer/Models/BookTitle.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSiteServer.Models
+{
+    public class BookTitle
+    {
+        [Key]
+        public int AltTitleId { get; set; }
+
+        public string Title { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
