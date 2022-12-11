fca62bda8a33a37977f3558bdc93fd622c9daf83
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-11 12:09:35 +0530

    add user model class

---
+++ BookSiteServer/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSiteServer.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? UserName { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Review> Reviews { get; set; }
+
+        public ICollection<Book> Library { get; set; }
+    }
+}
