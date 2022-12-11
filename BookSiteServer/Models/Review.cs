aa1626df7bc7f400434060e3b0bff5d1ccce03c1
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-11 12:10:12 +0530

    add review model class

---
+++ BookSiteServer/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSiteServer.Models
+{
+    public class Review
+    {
+        [Key]
+        public Guid ReviewId { get; set; }
+
+        public int Rate { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public User User { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
