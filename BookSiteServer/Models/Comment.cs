d7790c2cfac2079bcf7a84d0a5790176cb0af981
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-11 12:08:55 +0530

    add comment model class

---
+++ BookSiteServer/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSiteServer.Models
+{
+    public class Comment
+    {
+        [Key]
+        public Guid CommentId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public Book Book { get; set; }
+
+        public User User { get; set; }
+    }
+}
