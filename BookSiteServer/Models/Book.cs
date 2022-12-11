de09d6cd12c2dd3a73042feb6a6f099cee68a506
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-11 12:14:17 +0530

    add book model class

---
+++ BookSiteServer/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSiteServer.Models
+{
+    public class Book
+    {
+        [Key]
+        public Guid BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string ShortTitle { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Tag> Tag { get; set; }
+
+        public ICollection<BookTitle> AltTitle { get; set; }
+
+        public string Status { get; set; }
+
+        public int ChaptersNum { get; set; }
+
+        public int Views { get; set; }
+
+        public int Readers { get; set; }
+
+        public int Rate { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Review> Reviews { get; set; }
+
+        public ICollection<Chapter> Chapters { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
