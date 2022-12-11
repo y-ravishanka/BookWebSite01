c2018f078fd441480a4d93ea99dc9a4619315ecc
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-11 12:14:03 +0530

    add chapter model class

---
+++ BookSiteServer/Models/Chapter.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSiteServer.Models
+{
+    public class Chapter
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public int ChapterId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string? Content { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public Book Book { get; set; }
+    }
+}
