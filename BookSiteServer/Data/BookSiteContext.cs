13411e135f1c28f6c46880a43139f19b4cff1b4b
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-12 09:52:44 +0530

    add database context class

---
+++ BookSiteServer/Data/BookSiteContext.cs
@@ -0,0 +1,10 @@
+﻿using BookSiteServer.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BookSiteServer.Data
+{
+    public class BookSiteContext : DbContext
+    {
+    }
+}
