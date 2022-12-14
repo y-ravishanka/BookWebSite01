bdb799d0f6ae7805a57394a0e5524f8a56b16fee
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-14 08:35:31 +0530

    create DbContext class to create EF Core database

--- BookSiteServer/Data/BookSiteContext.cs
+++ BookSiteServer/Data/BookSiteContext.cs
@@ -6,5 +6,23 @@ namespace BookSiteServer.Data
 {
     public class BookSiteContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("BookSiteConnectionString"));
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<AuthorName> AuthorNames { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<BookTitle> BookTitles { get; set; }
+        public DbSet<Chapter> Chapters { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
