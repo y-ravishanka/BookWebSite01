c7fc134adb69813ee74e65e24cabcd819683410c
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-18 03:23:41 +0530

    add IChapterData to declare the methods interacting with Chapter data in the ef core database and ChapterData to impliment the IChapterData

---
+++ BookSiteServer/Data/ChaptersData.cs
@@ -0,0 +1,86 @@
+﻿using BookSiteServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookSiteServer.Data
+{
+    internal class ChaptersData : IChaptersData
+    {
+        private BookSiteContext context = new();
+
+        async Task<bool> IChaptersData.AddChapterAsync(Guid id, Chapter chapter)
+        {
+            try
+            {
+                Book book = await context.Books.Include(b => b.Chapters).SingleOrDefaultAsync(b => b.BookId == id);
+                if (book != null)
+                {
+                    book.Chapters.Add(chapter);
+                }
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return false;
+            }
+        }
+
+        async Task<Chapter> IChaptersData.GetChapterAsync(Guid id)
+        {
+            try
+            {
+                Chapter chapter = await context.Chapters.FirstOrDefaultAsync(c => c.Id == id);
+                if (chapter != null)
+                { return chapter; }
+                else
+                { return null; }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return null;
+            }
+        }
+
+        async Task<Chapter> IChaptersData.GetChapterAsync(string name, Guid id)
+        {
+            try
+            {
+                var chapter = from c in context.Chapters
+                              where c.Title == name && c.Book.BookId == id
+                              select c;
+
+                if (chapter != null)
+                { return (Chapter)chapter; }
+                else
+                { return null; }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return null;
+            }
+        }
+
+        async Task<bool> IChaptersData.UpdateChapterAsync(Chapter chapter)
+        {
+            try
+            {
+                Chapter chap = await context.Chapters.FirstOrDefaultAsync(c => c.Id == chapter.Id);
+                if (chap != null)
+                {
+                    chap.Title = chapter.Title;
+                    chap.Content = chapter.Content;
+                }
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return false;
+            }
+        }
+    }
+}
---
+++ BookSiteServer/Data/IChaptersData.cs
@@ -0,0 +1,12 @@
+﻿using BookSiteServer.Models;
+
+namespace BookSiteServer.Data
+{
+    internal interface IChaptersData
+    {
+        Task<bool> AddChapterAsync(Guid id, Chapter chapter);
+        Task<bool> UpdateChapterAsync(Chapter chapter);
+        Task<Chapter> GetChapterAsync(Guid id);
+        Task<Chapter> GetChapterAsync(string name, Guid id);
+    }
+}
