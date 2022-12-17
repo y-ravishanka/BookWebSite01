cf10129a8b4f7b836081a7ccb6af96b4759b8b39
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-18 03:30:09 +0530

    modify views acording to new interfaces

--- BookSiteServer/Views/Home/DisplayNovels.cshtml.js
+++ BookSiteServer/Views/Home/DisplayNovels.cshtml.js
@@ -1,1 +1,21 @@
 ﻿
+
+let novel0 = document.getElementById("novel0");
+let hello = document.getElementById("hello");
+let id01 = document.getElementById("id01");
+let id02 = document.getElementById("id02");
+
+novel0.addEventListener("click", function () {
+    alert("hello");
+    console.log("novel0 is working");
+});
+
+hello.addEventListener("click", function () {
+    alert("Hi");
+    console.log("hello is working")
+});
+
+id01.addEventListener("click", function () {
+    id02.textContent = "Hi !!!";
+    console.log("id01 is working")
+});
