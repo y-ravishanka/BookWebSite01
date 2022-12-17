08c855c7bc2296a6af541ccb8110f7c85aac2df3
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-18 03:34:52 +0530

    add and modify css and javascripts of the project

--- BookSiteServer/wwwroot/js/site.js
+++ BookSiteServer/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//let id01 = document.getElementById("id01");
+//let id02 = document.getElementById("id02");
+//let novel0 = document.getElementById("novel0");
+//let hello = document.getElementById("hello");
+
+//id01.addEventListener("click", function () {
+//    id02.textContent = "Hi !!!";
+//    console.log("id01 is working")
+//});
+
+//novel0.addEventListener("click", function () {
+//    alert("hello");
+//    console.log("novel0 is working");
+//    window.location.replace("ChapterView?id=2&name=Hello");
+//});
+
+//hello.addEventListener("click", function () {
+//    alert("Hi");
+//    console.log("hello is working")
+//});
+
+//for (let i = 1; i < 8; ++i) {
+//    let tmp01 = "novel" + i;
+//    document.getElementById("novel" + i).addEventListener("click", function () {
+//        alert(tmp01);
+//        console.log(tmp01);
+//    });
+//}
