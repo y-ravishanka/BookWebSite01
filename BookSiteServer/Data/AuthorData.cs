a04e34e60ceaa702318ef1e3f68fe295136739fe
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-17 11:11:09 +0530

    add IAuthorData interface to declare the methods interact author data with the ef core database add AuthorData class initialize declared methods of IAuthorData interface by inheriting the interface

---
+++ BookSiteServer/Data/AuthorData.cs
@@ -0,0 +1,169 @@
+﻿using BookSiteServer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace BookSiteServer.Data
+{
+    internal class AuthorData : IAuthorData
+    {
+        private BookSiteContext context = new();
+
+        async Task<bool> IAuthorData.AddAuthorAsync(Author author)
+        {
+            try
+            {
+                await context.Authors.AddAsync(author);
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return false;
+            }
+        }
+
+        async Task<Author> IAuthorData.GetAuthorAsync(Guid id)
+        {
+            try
+            {
+                var author = from a in context.Authors
+                             where a.AuthorId == id
+                             select a;
+                return (Author)author;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return null;
+            }
+        }
+
+        async Task<Author> IAuthorData.GetAuthorAsync(string name)
+        {
+            try
+            {
+                var author = from a in context.Authors
+                             from aa in a.AltName
+                             where a.Name == name || aa.Name == name
+                             select a;
+                return (Author)author;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return null;
+            }
+        }
+
+        async Task<List<Book>> IAuthorData.GetBooksAsync(Guid id)
+        {
+            try
+            {
+                var books = from a in context.Authors
+                            where a.AuthorId == id
+                            select a.Books;
+                return (List<Book>)books;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return null;
+            }
+        }
+
+        async Task<List<Book>> IAuthorData.GetBooksAsync(string name)
+        {
+            try
+            {
+                var books = from a in context.Authors
+                            from aa in a.AltName
+                            where a.Name.Equals(name) || aa.Name.Equals(name)
+                            select a.Books;
+                return (List<Book>)books;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return null;
+            }
+        }
+
+        async Task<bool> IAuthorData.UpdateAuthorAsync(Guid id, AuthorName authorName)
+        {
+            try
+            {
+                Author author = await context.Authors.Include(a => a.AltName).FirstOrDefaultAsync(a => a.AuthorId == id);
+                if (author is Author)
+                {
+                    author.AltName.Add(authorName);
+                }
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return false;
+            }
+        }
+
+        async Task<bool> IAuthorData.UpdateAuthorAsync(Guid id, Book book)
+        {
+            try
+            {
+                Author author = await context.Authors.Include(a => a.Books).FirstOrDefaultAsync(a => a.AuthorId == id);
+                if (author is Author)
+                {
+                    author.Books.Add(book);
+                }
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return false;
+            }
+        }
+
+        async Task<bool> IAuthorData.UpdateAuthorAsync(Guid id, string name)
+        {
+            try
+            {
+                Author author = await context.Authors.FirstOrDefaultAsync(a => a.AuthorId == id);
+                if (author is Author)
+                {
+                    author.Name = name;
+                }
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return false;
+            }
+        }
+
+        async Task<bool> IAuthorData.UpdateAuthorAsync(Guid id, int altId, string altname)
+        {
+            try
+            {
+                AuthorName author = await context.AuthorNames.FirstOrDefaultAsync(a => a.AltNameId == altId && a.Author.AuthorId == id);
+                if (author is AuthorName)
+                {
+                    author.Name = altname;
+                }
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.ToString() + "\n");
+                return false;
+            }
+        }
+    }
+}
---
+++ BookSiteServer/Data/IAuthorData.cs
@@ -0,0 +1,18 @@
+﻿using BookSiteServer.Models;
+
+namespace BookSiteServer.Data
+{
+    internal interface IAuthorData
+    {
+        Task<bool> AddAuthorAsync(Author author);
+        Task<bool> UpdateAuthorAsync(Guid id, string name);
+        Task<bool> UpdateAuthorAsync(Guid id, AuthorName authorName);
+        Task<bool> UpdateAuthorAsync(Guid id, Book book);
+        Task<bool> UpdateAuthorAsync(Guid id, int altId, string altname);
+        Task<Author> GetAuthorAsync(Guid id);
+        Task<Author> GetAuthorAsync(string name);
+        Task<List<Book>> GetBooksAsync(Guid id);
+        Task<List<Book>> GetBooksAsync(string name);
+        
+    }
+}
