7d827be844d339c999828640f2b5707dedc2096d
y-ravishanka <ravishanka.yasas@gmail.com>
2022-12-20 08:06:02 +0530

    add WarningController to direct Warning view directory views

---
+++ BookSiteServer/Controllers/WarningController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookSiteServer.Controllers
+{
+    public class WarningController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
